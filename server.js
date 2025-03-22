9df2d62cd92a1b2c579a34ec8ce312141ad5d461
NSaiEPS <sainizameps@gmail.com>
2025-03-22 13:28:18 +0530

    cors error handling

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.post("/user/login", (req, res) => {
 });
 */
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // // Use formidable middleware
 // app.use(formidable());
 
